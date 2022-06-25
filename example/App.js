635940c1e2751b7f705abbabfa1e5737740c8a2c
Michael Bilenko <denso.ffff@gmail.com>
2022-06-25 10:13:50 +0300

    Convert example project to RN 0.69

--- example/App.js
+++ example/App.js
@@ -8,7 +8,7 @@ import {
   Text,
   Switch,
 } from 'react-native';
-import {PROVIDER_GOOGLE, PROVIDER_DEFAULT} from '../index';
+import {PROVIDER_GOOGLE, PROVIDER_DEFAULT} from 'react-native-maps';
 import DisplayLatLng from './examples/DisplayLatLng';
 import ViewsAsMarkers from './examples/ViewsAsMarkers';
 import EventListener from './examples/EventListener';
@@ -182,8 +182,8 @@ export default class App extends React.Component<Props> {
         [CameraControl, 'CameraControl', true],
         [MassiveCustomMarkers, 'MassiveCustomMarkers', true],
         [GeojsonMap, 'Geojson', true],
-        [CacheURLTiles, 'CacheURLTiles', true ],
-        [CacheWMSTiles, 'CacheWMSTiles', true ],
+        [CacheURLTiles, 'CacheURLTiles', true],
+        [CacheWMSTiles, 'CacheWMSTiles', true],
       ]
         // Filter out examples that are not yet supported for Google Maps on iOS.
         .filter(
